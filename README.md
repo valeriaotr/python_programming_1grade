e30ac29fe912f09e9c01b91d323850828f93696d
valeriaotr <valeryaotr@gmail.com>
2024-06-02 21:44:14 +0300

    v1

--- README.md
+++
@@ -1,1 +0,0 @@
-# python_programming_1grade
--- vk api/vkapi/__init__.py
+++
@@ -1,4 +0,0 @@
-from homework08.vkapi import config  # type: ignore
-from homework08.vkapi.session import Session  # type: ignore
-
-session = Session(config.VK_CONFIG["domain"])
